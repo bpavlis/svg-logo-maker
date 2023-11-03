bb4eeda1d0abc94fd9d3ff58bc65e4d1ead4fa7c
bpavlis <benpavlis@gmail.com>
2023-11-02 22:08:03 -0500

    made test

--- shapes/shape.test.js
+++ shapes/shape.test.js
@@ -1,2 +1,34 @@
-const {Triangle, Square, Circle} = require("./shapes/shape-classes")
+const {Triangle, Square, Circle} = require("./shape-classes")
 
+describe('Shapes', () => {
+  describe('Triangle', () => {
+    it('should create a triangle with a blue background color', () => {
+      const shape = new Triangle("blue", "triangle");
+      expect(shape.color).toEqual("blue");
+    })
+  })
+
+  describe('Square', () => {
+    it('should create a square with a blue background color', () => {
+      const shape = new Square("blue", "square");
+      expect(shape.color).toEqual("blue");
+    })
+  })
+
+  describe('Circle', () => {
+    it('should create a circle with a blue background color', () => {
+      const shape = new Circle("blue", "circle");
+      expect(shape.color).toEqual("blue");
+    })
+  })
+
+
+})
+
+
+
+/*
+const shape = new Triangle();
+shape.setColor("blue");
+expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+*/
